f02b3f8b73a17af736ce2d13b71fc8210998bf76
Soroush Borzoo <94343205+SoVaBo@users.noreply.github.com>
2022-03-07 07:35:06 +0000

    speech to text

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ recognizer = Recognizer()
 with AudioFile('chile.wav') as audio_file:
   audio = recognizer.record(audio_file)
 
-text  = recognizer.recognize_google()
+text  = recognizer.recognize_google(audio)
 print(text)
